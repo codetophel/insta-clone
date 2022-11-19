11da5fd644f80b3f21f9a88a5e7495ddaeb12b23
codetophel <nicodemusakande@gmail.com>
2022-11-19 17:07:24 +0100

    modified:   pages/components/Suggestion.js

--- pages/components/Suggestion.js
+++ pages/components/Suggestion.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const Suggestion = ({ avatar, email, username }) => {
+const Suggestion = ({ img, email, username }) => {
   return (
     <div className='flex items-center justify-between mt-3'>
-      <img
-        className='rounded-full w-10 h-10 border p[-2px]'
-        src={avatar}
-        alt=''
-      />
+      <img className='rounded-full w-10 h-10 border p[-2px]' src={img} alt='' />
       <div className='flex-1 ml-4'>
         <h2 className='font-semibold text-sm'>{username}</h2>
         <h3 className='text-sm text-gray-400 truncate'> Email: {email}</h3>
